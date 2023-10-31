f86826aa2ad6f12767ca1dd0c2aeca7de03c6d51
Maxim Sinitsyn <72615475+sinitsynme@users.noreply.github.com>
2023-11-01 02:24:09 +0400

    LOGAPI4 - Configure Gradle and base dependencies (#1)
    * LOGAPI4 - Configure build.gradle's
    
    * LOGAPI4 - Add simple test
    
    * LOGAPI4 - Add simple test failure
    
    * LOGAPI4 - Add SpringBoot context tests

--- product/build.gradle.kts
+++ product/build.gradle.kts
@@ -1,16 +1,29 @@
 plugins {
+    id("org.springframework.boot") version("2.7.2")
+    id("io.spring.dependency-management") version("1.0.11.RELEASE")
     id("java")
 }
 
-group = "ru.sinitsynme"
+group = "ru.sinitsynme.logistapi"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    val mapstructVersion = "1.3.1.Final"
+
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.mapstruct:mapstruct:${mapstructVersion}")
+
+    annotationProcessor("org.mapstruct:mapstruct-processor:${mapstructVersion}")
+    compileOnly("org.mapstruct:mapstruct-processor:${mapstructVersion}")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
