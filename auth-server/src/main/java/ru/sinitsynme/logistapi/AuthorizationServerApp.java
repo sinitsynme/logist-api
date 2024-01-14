cc9fec5ceefd7af552e528697195562202d92170
Maxim Sinitsyn <72615475+sinitsynme@users.noreply.github.com>
2024-01-14 20:54:59 +0400

    [LG-52] Add dependencies and basic classes to AuthService (#17)

---
+++ auth-server/src/main/java/ru/sinitsynme/logistapi/AuthorizationServerApp.java
@@ -0,0 +1,11 @@
+package ru.sinitsynme.logistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApp.class, args);
+    }
+}
