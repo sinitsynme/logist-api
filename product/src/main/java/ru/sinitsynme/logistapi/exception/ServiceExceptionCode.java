cb9854cacf5d4e789b458b418265952027db50a5
sinitsynme <sinitsynme@outlook.com>
2024-03-01 23:50:48 +0400

    [LG-68] Add feature settings to product

--- product/src/main/java/ru/sinitsynme/logistapi/exception/ServiceExceptionCode.java
+++ product/src/main/java/ru/sinitsynme/logistapi/exception/ServiceExceptionCode.java
@@ -12,5 +12,7 @@ public final class ServiceExceptionCode {
     public static final String ILLEGAL_FILE_UPLOAD_CODE = "-400";
     public static final String PRODUCT_IMAGE_NOT_FOUND = "-401";
     public static final String PRODUCT_IMAGE_BROKEN_READ = "-402";
+    public static final String FEATURE_SETTING_EXISTS_CODE = "-500";
+    public static final String FEATURE_SETTING_NOT_FOUND_CODE = "-501";
 
 }
---
+++ product/src/main/resources/db/migration/V4__Add_feature_setting_table.sql
@@ -0,0 +1,1 @@
+create table feature_setting (name varchar(255) not null, description varchar(255) not null, enabled boolean not null, primary key (name));
